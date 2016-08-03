d2de366b82e7299f64213b69701e642fabbb33dc
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2016-08-04 00:56:27 +0300

    chore(angular2): simple frontend

---
+++ src/polyfills.ts
@@ -0,0 +1,7 @@
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+// Typescript emit helpers polyfill
+import 'ts-helpers';
