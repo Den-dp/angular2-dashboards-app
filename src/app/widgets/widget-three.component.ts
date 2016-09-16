1dd1ac4c140d999e15aecc8925fcf152c338485b
Denis Bendrikov <Denis.Bendrikov@gmail.com>
2016-09-16 03:35:41 +0300

    chore(mastering): provider example

--- src/app/widgets/widget-three.component.ts
+++ src/app/widgets/widget-three.component.ts
@@ -1,14 +1,27 @@
 import {Component, ViewChild, Renderer, Input} from "@angular/core";
+import {LogDebugger} from "../services/log-debugger.service";
 @Component({
   selector: 'widget-three',
-  template: `<input #input type="text" [value]="message">`
+  template: `<input #input type="text" [value]="message">`,
+  providers: [
+    {
+      provide: LogDebugger,
+      useFactory: ()=> {
+        return new LogDebugger(true);
+      }
+    }
+  ]
 })
 export class WidgetThree {
   @ViewChild('input') input;
 
   @Input() message = 'default value';
 
-  constructor(private renderer:Renderer){}
+  constructor(private renderer:Renderer, private logDebugger: LogDebugger){}
+
+  ngOnInit(){
+    this.logDebugger.debug('Getting...');
+  }
 
   ngAfterViewInit(){
     console.log(this.input);
